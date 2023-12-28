00d270e69fdec49301af0fe604778189cf0dcac8
Vlad Chepaykyn <backdround@yandex.ru>
2023-12-28 10:58:05 +0300

    Add search previous pattern function

--- lua/pattern-iterator/search-pattern.lua
+++ lua/pattern-iterator/search-pattern.lua
@@ -90,6 +90,41 @@ local search_next = function(pattern, relative_position)
   }
 end
 
+---Searches a previous pattern match from the given position.
+---relative_position is exclusive.
+---@param pattern string
+---@param relative_position PI_Position
+---@return PI_PatternPosition?
+local search_previous = function(pattern, relative_position)
+  relative_position.set_cursor()
+
+  local offset = 1
+  local end_pattern_position = nil
+  while true do
+    local potential_end_pattern_position = vim_search(pattern, "beWn")
+    if potential_end_pattern_position == nil then
+      return nil
+    end
+
+    if relative_position ~= potential_end_pattern_position then
+      end_pattern_position = potential_end_pattern_position
+      break
+    end
+
+    potential_end_pattern_position.move(-offset)
+    potential_end_pattern_position.set_cursor()
+  end
+
+  end_pattern_position.set_cursor()
+  local start_pattern_position = vim_search(pattern, "bcWn")
+
+  return {
+    start_position = start_pattern_position,
+    end_position = end_pattern_position,
+  }
+end
+
+
 ---Sets vim state for searching.
 ---@return function It restores the initial vim state.
 local prepare_vim_state = function()
@@ -136,6 +171,20 @@ M.next = function(pattern, relative_position)
   local restore_vim_state = prepare_vim_state()
 
   local result = { search_next(pattern, relative_position) }
+
+  restore_vim_state()
+  return unpack(result)
+end
+
+---Searches a previous pattern match from the given position.
+---relative_position is exclusive.
+---@param pattern string
+---@param relative_position PI_Position
+---@return PI_PatternPosition?
+M.previous = function(pattern, relative_position)
+  local restore_vim_state = prepare_vim_state()
+
+  local result = { search_previous(pattern, relative_position) }
 
   restore_vim_state()
   return unpack(result)

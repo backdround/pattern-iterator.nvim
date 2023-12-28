45b4e1d49759fb76fd343767601ba97a9a09c5a1
Vlad Chepaykyn <backdround@yandex.ru>
2023-12-28 07:24:11 +0300

    Add search next pattern function

--- lua/pattern-iterator/search-pattern.lua
+++ lua/pattern-iterator/search-pattern.lua
@@ -54,6 +54,42 @@ local search_current = function(pattern, relative_position)
   }
 end
 
+---Searches the pattern from the given position. from_position is exclusive.
+---@param pattern string
+---@param relative_position PI_Position
+---@return PI_PatternPosition?
+local search_next = function(pattern, relative_position)
+  relative_position.set_cursor()
+
+  local start_pattern_position = vim_search(pattern, "cWn")
+  if start_pattern_position == nil then
+    return nil
+  end
+
+  if start_pattern_position == relative_position then
+    start_pattern_position = vim_search(pattern, "Wn")
+    if start_pattern_position == nil then
+      return nil
+    end
+  end
+
+  local end_pattern_position = vim_search(pattern, "ecWn")
+
+  while end_pattern_position < start_pattern_position do
+    if end_pattern_position == nil then
+      return nil
+    end
+    end_pattern_position.set_cursor()
+    end_pattern_position = vim_search(pattern, "eWn")
+  end
+
+  return {
+    start_position = start_pattern_position,
+    end_position = end_pattern_position,
+  }
+end
+
+
 ---Sets vim state for searching
 ---@return function It restores the initial vim state.
 local prepare_vim_state = function()
@@ -83,10 +119,22 @@ local M = {}
 ---@param relative_position PI_Position
 ---@return PI_PatternPosition?
 M.current = function(pattern, relative_position)
-
   local restore_vim_state = prepare_vim_state()
 
   local result = { search_current(pattern, relative_position) }
+
+  restore_vim_state()
+  return unpack(result)
+end
+
+---Searches the pattern from the given position. from_position is exclusive.
+---@param pattern string
+---@param relative_position PI_Position
+---@return PI_PatternPosition?
+M.next = function(pattern, relative_position)
+  local restore_vim_state = prepare_vim_state()
+
+  local result = { search_next(pattern, relative_position) }
 
   restore_vim_state()
   return unpack(result)
